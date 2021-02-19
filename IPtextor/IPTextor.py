cc69f45447d76e8059c28aa0d520a5462f4f3105
Aidanhall34 <Aidan.hall34@outlook.com>
2021-02-19 23:05:13 +1100

    Better logging on the service

--- IPtextor/IPTextor.py
+++ IPtextor/IPTextor.py
@@ -1,33 +1,45 @@
 #!/bin/python3
-#This script requires a twilio account, you can set this up here
-#https://www.twilio.com/try-twilio
-#See the Twilio doc for obtaining auth tokens
-#See the read me for general information about twilio
+#This is meant to be run as a cron job.
+#It will find you public IP address and send it to you in a text message
+#If you enable port forwarding on some devices you can phone home without paying
+#For a static IP :)
+#You will need to set up a way to send a text, I will be using twilo
 import time
 import requests
 import os
 from twilio.rest import Client
 from contextlib import redirect_stdout
 import datetime 
-
+import logging
+from logging.handlers import RotatingFileHandler
 def __main__():
 #Defining the text message Function, change the number and auth stuff as needed
     ip = requests.get('https://api.ipify.org').text
-
+    
     def sendmessage():
-            #Change these
+        #Change this
             sid = '' 
             auth_token = ''
             client = Client(sid, auth_token)
         
             message = client.messages.create(
-            #Change these
             from_="",
             to="",    
             body="My home IP is: " + ip
             )            
             return print(message.sid)
-#This bit of the scipt can be left untouched
+    #no changes from here
+    log_format =logging.Formatter('%(asctime)s %(message)s', datefmt='%d/%m/%Y %I:%M:%S %p')
+    logfile = 'log.log'
+    my_handler = RotatingFileHandler(logfile, mode='a', maxBytes=5*1024*1024,
+        backupCount=2, encoding=None, delay=0)
+    my_handler.setFormatter(log_format)
+    my_handler.setLevel(logging.INFO)
+    app_log = logging.getLogger('root')
+    app_log.setLevel(logging.INFO)
+
+    app_log.addHandler(my_handler)
+
     def makeipfile():
         f = open("IP.txt", "w+")
         f.write(ip)
@@ -35,23 +47,13 @@ def __main__():
 
 # Check if this has been done before
     IPthere = os.path.exists("IP.txt")
-    logthere = os.path.exists("log.txt")
 
 
     def makeipfile():
         f = open("IP.txt", "w+")
         f.write(ip)
         f.close()
-
-
-    if logthere:
-        f = open("log.txt", "a+")
-        f.write(str(datetime.datetime.now()) + ": " + ip + "\n" )
-        f.close()
-    else:
-        f = open("log.txt", "w+")
-        f.write(str(datetime.datetime.now()) + ": " + ip + "\n")
-        f.close()
+    app_log.info(ip)
 
     if IPthere:
         print("file is there")
@@ -73,6 +75,6 @@ def __main__():
         sendmessage()
 while True:
     __main__()
-    time.sleep(600)
+  #  time.sleep(600)
     
 
