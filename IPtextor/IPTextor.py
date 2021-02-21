cde24ec0d9b593d2c737b8cc8f5ce071909ad75c
Aidanhall34 <Aidan.hall34@outlook.com>
2021-02-22 00:31:11 +1100

    IPTextor now has a rotating error log file as well as exception handling for all web requests

--- IPtextor/IPTextor.py
+++ IPtextor/IPTextor.py
@@ -1,80 +1,86 @@
-#!/bin/python3
-#This is meant to be run as a cron job.
-#It will find you public IP address and send it to you in a text message
-#If you enable port forwarding on some devices you can phone home without paying
-#For a static IP :)
-#You will need to set up a way to send a text, I will be using twilo
-import time
 import requests
-import os
-from twilio.rest import Client
-from contextlib import redirect_stdout
-import datetime 
 import logging
-from logging.handlers import RotatingFileHandler
+import os
+import datetime
+import time
+import logging.handlers as handlers 
+from twilio.rest import Client
+
+def sendmessage(ip):
+    #Change this
+    sid = ''
+    auth_token = ''
+    client =  Client(sid, auth_token)
+    message = client.messages.create(
+    #Change this
+    from_='',
+    to='',
+    body="My home IP is: " + ip)
+
+# Start of exception and HTTP logging
+loggers = {}
+formatter = logging.Formatter('%(asctime)s : %(message)s', datefmt='%d/%m/%y %H:%M:%S %p' )
+# logger = logging.basicConfig(filename='HTTPandERROR.log', level=logging.DEBUG,
+#             format='%(asctime)s : %(message)s', datefmt='%d/%m/%y %H:%M:%S %p' )
+
+logger = logging.getLogger("log")
+logger.setLevel(logging.INFO)
+handler = handlers.RotatingFileHandler("ERROR.log", maxBytes=5*1024*1024, backupCount=3)
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+#Create the referanced IP file
+def makeipfile(IP):
+    f = open("IP.txt", "w+")
+    f.write(IP)
+    f.close()
+
+#Create the IP address log file
+def writetoIPlog(IP):
+    with open("IP.log", "a+") as fp:
+        fp.write(str(datetime.datetime.now()) + " : " + IP + '\n')
+
 def __main__():
-#Defining the text message Function, change the number and auth stuff as needed
-    ip = requests.get('https://api.ipify.org').text
-    
-    def sendmessage():
-        #Change this
-            sid = ''
-            auth_token = ''
-            client = Client(sid, auth_token)
-        
-            message = client.messages.create(
-            from_="",
-            to="",    
-            body="My home IP is: " + ip
-            )            
-            return print(message.sid)
-    #no changes from here
-    log_format =logging.Formatter('%(asctime)s %(message)s', datefmt='%d/%m/%Y %I:%M:%S %p')
-    logfile = 'log.log'
-    my_handler = RotatingFileHandler(logfile, mode='a', maxBytes=5*1024*1024,
-        backupCount=2, encoding=None, delay=0)
-    my_handler.setFormatter(log_format)
-    my_handler.setLevel(logging.INFO)
-    app_log = logging.getLogger('root')
-    app_log.setLevel(logging.INFO)
 
-    app_log.addHandler(my_handler)
+    # Attempt to obtain your Public IP
+    try:
+        URL = 'https://api.ipify.org'  
+        ip = requests.get(URL).text
+    except Exception as e:
+        logger.error("Exception occured", exc_info=True)
+        print("The IP API server could not be reached.\nPlease ensure you and it are online")
+        print("The URL is: " + URL )
+        exit()
 
-    def makeipfile():
-        f = open("IP.txt", "w+")
-        f.write(ip)
-        f.close()
 
-# Check if this has been done before
+    writetoIPlog(ip)
+
+
     IPthere = os.path.exists("IP.txt")
-
-
-    def makeipfile():
-        f = open("IP.txt", "w+")
-        f.write(ip)
-        f.close()
-    app_log.info(ip)
-
     if IPthere:
         print("file is there")
         ipcheck = open("IP.txt", "r")
         Knownip = ipcheck.read()
-
         if Knownip == ip:
             print("We know about this one, no need to message")
         else:
             print("We got a new one!!")
             print("Sending message")
-            makeipfile()
-            sendmessage()
-
+            makeipfile(ip)
+            try:
+                sendmessage(ip)
+            except Exception as e:
+                logger.error("Exception occured", exc_info=True)
+                print("Unable to send the message.\nCheck the ERROR.log for more detail")
     else:
         print("You haven't done this before")
         print("Sending SMS")
-        makeipfile()
-        sendmessage()
+        makeipfile(ip)
+        try:
+            sendmessage(ip)
+        except Exception as e:
+            logger.error("Exception occured", exc_info=True)
+            print("Unable to send the message.\nCheck the HTTPandERROR.log for more detail")
 while True:
     __main__()
     time.sleep(600)
-    
-
